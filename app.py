60a51464edf03e93a4a99d2e1b8eaae8021f93bb
Saravana-444 <saravanak88088@gmail.com>
2025-12-19 21:03:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,32 @@ import streamlit as st
 import pickle
 import numpy as np
 
-st.set_page_config(page_title="Adult Income Prediction", layout="centered")
-st.title("💼 Adult Income Census Prediction")
+# Load files
+with open("adult_income_model.pkl", "rb") as f:
+    model = pickle.load(f)
 
-@st.cache_resource
-def load_model():
-    with open("Adult_Income.pkl", "rb") as f:
-        return pickle.load(f)
+with open("encoder.pkl", "rb") as f:
+    encoder = pickle.load(f)
 
-model = load_model()
+with open("scaler.pkl", "rb") as f:
+    scaler = pickle.load(f)
 
-age = st.number_input("Age", 17, 100, 30)
-education_num = st.number_input("Education Number", 1, 16, 9)
-capital_loss = st.number_input("Capital Loss", 0, 10000, 0)
-hours_per_week = st.number_input("Hours per Week", 1, 100, 40)
+st.title("Adult Income Prediction")
+
+age = st.number_input("Age", 18, 100)
+education = st.selectbox("Education", ["Bachelors", "HS-grad", "Masters"])
+occupation = st.selectbox("Occupation", ["Tech-support", "Craft-repair", "Sales"])
+hours = st.number_input("Hours per week", 1, 100)
 
 if st.button("Predict Income"):
-    input_data = np.array([[age, education_num, capital_loss, hours_per_week]])
+    cat_data = encoder.transform([[education, occupation]])
+    num_data = scaler.transform([[age, hours]])
+
+    input_data = np.hstack((num_data, cat_data))
+
     prediction = model.predict(input_data)
 
     if prediction[0] == 1:
-        st.success("✅ Income: >50K")
+        st.success("Income > 50K")
     else:
-        st.success("✅ Income: ≤50K")
+        st.warning("Income ≤ 50K")
